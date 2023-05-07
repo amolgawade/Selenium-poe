63d07e7682d1cb43035ee1fe0f2209972aebfaec
Amol <amolsg863@gmail.com>
2023-05-08 00:24:45 +0530

    save to excel done

--- src/defFunction.py
+++ src/defFunction.py
@@ -1,3 +1,4 @@
+from openpyxl import Workbook
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from openpyxl.worksheet import worksheet
@@ -46,39 +47,67 @@ class PoeSite:
         self.driver.quit()
 
     def ask_questions(self):
+         # Create a Workbook instance
+        workbook = Workbook()
+        sheet = workbook.active
 
-
-        time.sleep(5)
-        text_area = self.driver.find_element(*HomePageLocators.question_box).click()
+        # Load the Excel file
         workbook = openpyxl.load_workbook(file_name)
         worksheet = workbook['Sheet1']
         max_row = worksheet.max_row
-
-        for row_number in range(1, max_row):
+        self.driver.find_element(By.XPATH, "//*[@id='__next']/div[1]/section/footer/div/div[2]/textarea").click()
+        # Loop through each row in the Excel file
+        for row_number in range(1, max_row + 1):
             question = worksheet.cell(row=row_number, column=1).value
 
-            self.driver.find_element(*HomePageLocators.question_box).send_keys(question)
-            self.driver.find_element(*HomePageLocators.question_send).click()
+            # Click on the text area
+
+
+            # Enter the question in the text area
+            self.driver.find_element(By.XPATH, "//*[@id='__next']/div[1]/section/footer/div/div[2]/textarea").send_keys(question)
+
+            # Click on the send button
+            self.driver.find_element(By.XPATH, "//*[@id='__next']/div[1]/section/footer/div/div[3]/button").click()
+
             time.sleep(10)
 
-            # wait for visible the like button
+            # Wait for the like button to be visible
             wait = WebDriverWait(self.driver, timeout=100)
             index = 0 + row_number
             wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='__next']/div[1]/section/div/div/div[{}]/section/button[2]".format(index))))
 
-            # hover to element
-            time.sleep(0.5)
-            element = self.driver.find_element(By.XPATH,"//*[@id='__next']/div[1]/section/div/div/div[{}]/div[2]/div[3]/div/div/button/div".format(index))
+            wait = WebDriverWait(self.driver, timeout=20)
+            hover_box = "//*[@id='__next']/div[1]/section/div/div/div[{}]/div[2]/div[2]/div[1]/div[1]".format(index)
+            box = wait.until(EC.visibility_of_element_located((By.XPATH, hover_box)))
             actions = ActionChains(self.driver)
-            actions.move_to_element(element).click()
+            actions.move_to_element(box).click().perform()
 
-            # copy button
-            time.sleep(0.5)
-            data = self.driver.find_element(By.XPATH, "//*[@id='__next']/div[1]/section/div/div/div[{}]/div[2]/div[3]/div/div/div/div/button[3]".format(index)).click()
-            
-            # copy data to clipboard
-            self.clipboard = pyperclip
-            self.clipboard.copy(data)
+            # hover to drop dowm menu
+            wait = WebDriverWait(self.driver, timeout=20)
+            hover_xpath = "//*[@id='__next']/div[1]/section/div/div/div[{}]/div[2]/div[3]/div/div/button/div".format(index)
+            hover = wait.until(EC.visibility_of_element_located((By.XPATH, hover_xpath)))
+
+            actions = ActionChains(self.driver)
+            actions.move_to_element(hover).click().perform()
+
+            # Click on the copy button
+            wait = WebDriverWait(self.driver, timeout=20)
+            copy_but = "//*[@id='__next']/div[1]/section/div/div/div[{}]/div[2]/div[3]/div/div/div/div/button[3]".format(index)
+            copy = wait.until(EC.visibility_of_element_located((By.XPATH, copy_but)))
+            copy.click()
+
+            # Copy data from clipboard
+            clipboard = pyperclip
+            copied_data = clipboard.paste()
+
+            # Write data to Excel file
+            worksheet.cell(row=row_number, column=2).value = copied_data
+            # Reset clipboard
+            clipboard.copy('')
+
+        # Save the Excel file
+        workbook.save(file_name)
+
 
 
 
